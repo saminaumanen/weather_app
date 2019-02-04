7de1fd392f8b62cdd11950c58bb4b8192b0fb267
saminaumanen <naumanen.sami@gmail.com>
2019-02-04 19:00:00 +0200

    component testing and snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import SearchWeather from './SearchWeather';
+import Weather from './Weather';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('shallow component test', ()=>{
+  shallow(<App/>);
+})
+
+it('shallow SearchWeather component test', ()=>{
+  shallow(<SearchWeather/>);
+})
+
+it('shallow Weather component test', ()=>{
+  shallow(<Weather/>);
+})
+
+it('renders correctly', ()=>{
+  const tree = renderer.create(<App/>).toJSON();
+  expect(tree).toMatchSnapshot();
+})
